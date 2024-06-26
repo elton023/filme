4ce22c7f19d115111e14e4d4482d9e2a2d88127b
elton023 <159277209+elton023@users.noreply.github.com>
2024-06-26 20:55:47 -0300

    inicio css

--- cadastro_filmes.php
+++ cadastro_filmes.php
@@ -3,9 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Filmes</title>
-    <link rel="stylesheet" href="style.css">
-
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Cadastro de Filmes</title>   
 </head>
 <body>
     <div class="container">
